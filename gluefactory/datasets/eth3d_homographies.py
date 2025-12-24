4ee8e7aa41b298deb48e169465ecafd484d4e89d
Ryan Soussan <ryan.soussan@gmail.com>
2025-12-23 17:09:11 -0800

    fixed shutil compatability issue

--- gluefactory/datasets/eth3d_homographies.py
+++ gluefactory/datasets/eth3d_homographies.py
@@ -253,7 +253,7 @@ class ETH3DHomographyDataset(BaseDataset):
         torch.hub.download_url_to_file(url_base + zip_name, zip_path)
         with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(tmp_dir)
-        shutil.move(tmp_dir / zip_name.split(".")[0], data_dir)
+        shutil.move(str(tmp_dir / zip_name.split(".")[0]), str(data_dir))
 
     def get_dataset(self, split):
         return self
